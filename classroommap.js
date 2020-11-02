b7fada8f09072e43d8099eba44bcdf9e19deed9f
Yey007 <yey007tech@gmail.com>
2020-11-02 16:21:15 -0500

    Added requirements for Adam

---
+++ classroommap.js
@@ -0,0 +1,5 @@
+// Implement a map of classroom ids (ints or strings) to the number of people who have their hand raised (int)
+// Three functions: 
+// 1. raiseHand(classroomid) -> Increments the raised count for a classroomid
+// 2. lowerHand(classroomid) -> Decrements the raised count for a classroomid
+// 3. hasHandRaised(classroomid) -> returns whether anyone ahs their hand raised
