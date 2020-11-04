bc1359138e4f3e384593b7ca5154854f5cfdd53a
Andihedral <adamymz@gmail.com>
2020-11-04 15:59:42 -0500

    This better work

--- classroommap.js
+++ classroommap.js
@@ -1,5 +1,31 @@
-// Implement a map of classroom ids (ints or strings) to the number of people who have their hand raised (int)
-// Three functions: 
-// 1. raiseHand(classroomid) -> Increments the raised count for a classroomid
-// 2. lowerHand(classroomid) -> Decrements the raised count for a classroomid
-// 3. hasHandRaised(classroomid) -> returns whether anyone ahs their hand raised
+// Implement a map of classroom ids (ints or strings) to the number of people who have their hand raised
+// Three functions:
+// 1. raiseHand(classroomid) -> Implements the raised count for a classroomid
+// 2. lowerHand(classroomid) -> Decrements the raised a classroomid
+// 3. hasHandRaised(classroomid) -> returns whether anyone has their hand raised.
+
+
+
+let classId = new Map();
+
+
+function raiseHand(classroomid) {
+    let handRaise = classId.get(classroomid);
+    classId.set('classroomId', handRaise + 1);
+}
+
+function lowerHand(classroomid) {
+    let handRaise = classId.get(classroomid);
+    classId.set('classroomId', handRaise - 1);
+}
+
+function hasHandRaised(classroomid) {
+    if (classId.get(classroomid) > 0) {
+        return true;
+    }
+    return false;
+}
+
+exports.raiseHand = raiseHand;
+exports.lowerHand = lowerHand;
+exports.hasHandRaised = hasHandRaised;
