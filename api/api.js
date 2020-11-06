099518442a6955bee9199abad09e24aee3ff7249
Yey007 <yey007tech@gmail.com>
2020-11-06 16:06:02 -0500

    Bug fixes for classroommap.js and new endpoint

--- api/api.js
+++ api/api.js
@@ -1,8 +1,15 @@
+const classroomMap = require("../classroommap")
 const idGen = require("./generateId")
 
 function init(app) {
+
     app.get("/api/generateId", function(req, res) {
         res.send({id: idGen.next()})
+    })
+
+    app.get("/api/handRaised/:classid", function(req, res) {
+        let isRaised = classroomMap.hasHandRaised(req.params.classid)
+        res.send({raised: isRaised})
     })
 }
 
--- classroommap.js
+++ classroommap.js
@@ -3,12 +3,20 @@ let classId = new Map();
 //raiseHand(classroomid) -> Implements the raised count for a classroomid
 function raiseHand(classroomid) {
     let handRaise = classId.get(classroomid);
-    classId.set('classroomId', handRaise + 1);
+    if(handRaise === undefined) {
+        handRaise = 0
+    }
+    classId.set(classroomid, handRaise + 1);
 }
 //lowerHand(classroomid) -> Decrements the raised a classroomid
 function lowerHand(classroomid) {
     let handRaise = classId.get(classroomid);
-    classId.set('classroomId', handRaise - 1);
+    if(handRaise === undefined) {
+        handRaise = 0
+    }
+    if(handRaise !== 0) {
+        classId.set(classroomid, handRaise - 1);
+    }
 }
 //hasHandRaised(classroomid) -> returns whether anyone has their hand raised.
 function hasHandRaised(classroomid) {
