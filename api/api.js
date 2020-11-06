fa1d139026b6d1794c75f34455d7f1e8d94b2d09
Yey007 <yey007tech@gmail.com>
2020-11-06 16:09:17 -0500

    Renamed some stuff and added comments to make more clear

--- api/api.js
+++ api/api.js
@@ -3,10 +3,12 @@ const idGen = require("./generateId")
 
 function init(app) {
 
+    //Generates a new classroom id
     app.get("/api/generateId", function(req, res) {
         res.send({id: idGen.next()})
     })
 
+    //Returns wether the robotic hand should be raised
     app.get("/api/handRaised/:classid", function(req, res) {
         let isRaised = classroomMap.hasHandRaised(req.params.classid)
         res.send({raised: isRaised})
