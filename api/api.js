83dc4f0699519b78b38a67b6150dcddadb0c0aae
ecukier <ephramcukier@icloud.com>
2020-11-09 22:06:01 -0500

    changed get request to return value of isRaised from data.json. This is to make prototyping faster

--- api/api.js
+++ api/api.js
@@ -1,17 +1,18 @@
-const classroomMap = require("../classroommap")
-const idGen = require("./generateId")
+const classroomMap = require("../classroommap");
+const idGen = require("./generateId");
+const editJSON = require("edit-json-file");
 
 function init(app) {
 
     //Generates a new classroom id
     app.get("/api/generateId", function(req, res) {
-        res.send({id: idGen.next()})
+        res.send({id: idGen.next()});
     })
 
     //Returns whether the robotic hand should be raised
     app.get("/api/handRaised/:classid", function(req, res) {
-        let isRaised = classroomMap.hasHandRaised(req.params.classid)
-        res.send({raised: isRaised})
+        let isRaised = classroomMap.hasHandRaised(req.params.classid);
+        res.send({raised: editJSON('data.json').get("isRaised")});
     })
 }
 
--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const user = require('./user/user')
 api.init(app)
 user.init(app)
 
-app.listen(PORT, function(err){
-    if(err){
-        console.log(err);
-    } else {
-        console.log("Listening on localhost:" + PORT);
-        open('http://localhost:' + PORT);
-    }
+app.listen(PORT, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Listening on localhost:" + PORT);
+    open('http://localhost:' + PORT);
+  }
 });
