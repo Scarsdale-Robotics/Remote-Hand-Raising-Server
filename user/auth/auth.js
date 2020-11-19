1ab705b9cfc57d5e4f01d927913a48e08f616d25
ecukier <ephramcukier@icloud.com>
2020-11-18 20:22:45 -0500

    fixed absolute (localhost) path in a rediret (WHO ADDED THIS IN HERE???)

--- user/auth/auth.js
+++ user/auth/auth.js
@@ -26,7 +26,7 @@ function init(app) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:5000/auth/google/callback"
+        callbackURL: "/auth/google/callback"
     },
         function (accessToken, refreshToken, profile, done) {
           console.log('reeeeeeeeeee')
